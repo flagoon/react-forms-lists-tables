7524af1ea250e98fa3da53e1ef2fd0429b062f73
Paweł Kochanek <flagoon@o2.pl>
2018-03-07 23:07:16 +0100

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './../assets/scss/App.scss';
+
+const App = (props: {}) => (
+    <div className="app">
+        <h1>Hello World!!</h1>
+        <p>Foo to the barz</p>
+    </div>
+);
+
+export default App;
